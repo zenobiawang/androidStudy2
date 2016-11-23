3601c4ef391892fb201aaff8be055c8239c14566
wanghui1 <huia.wang@quantgroup.cn>
2016-11-23 16:51:52 +0800

    同步

---
+++ MyApplication/app/src/main/java/com/example/wanghui1/androidstudy/resoucecode/cache/DiskLruCacheDemoActivity.java
@@ -0,0 +1,23 @@
+package com.example.wanghui1.androidstudy.resoucecode.cache;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.wanghui1.androidstudy.base.BaseActivity;
+
+/**
+ * Created by wanghui on 2016/11/23.
+ */
+
+public class DiskLruCacheDemoActivity extends BaseActivity {
+    private String IMG_URL = "http://pic37.nipic.com/20140102/15590187_203601118161_2.jpg";
+    @Override
+    protected void init(Bundle savedInstanceState, View view) {
+
+    }
+
+    @Override
+    protected int getContentLayout() {
+        return 0;
+    }
+}
