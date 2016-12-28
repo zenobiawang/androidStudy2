d04e486b9fd1e0eaeba1a9f2b0a5f19c61014e73
wanghui1 <huia.wang@quantgroup.cn>
2016-12-28 11:15:45 +0800

    viewDragHelper使用最终版本

--- MyApplication/app/src/main/java/com/example/wanghui1/androidstudy/design/view/ViewDragHelperDemo.java
+++ MyApplication/app/src/main/java/com/example/wanghui1/androidstudy/design/view/ViewDragHelperDemo.java
@@ -8,7 +8,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import com.example.wanghui1.androidstudy.R;
 
 /**
  * Created by wanghui on 2016/12/26.
@@ -17,8 +16,6 @@ import com.example.wanghui1.androidstudy.R;
 public class ViewDragHelperDemo extends LinearLayout{
     private static String TAG = "ViewDragHelperDemo";
     private final ViewDragHelper mDragHelper;
-    private View mDragView1;
-    private View mDragView2;
 
     public ViewDragHelperDemo(Context context) {
         this(context, null);
@@ -33,12 +30,6 @@ public class ViewDragHelperDemo extends LinearLayout{
         mDragHelper = ViewDragHelper.create(this, 1f, new DragHelperCallback());
     }
 
-    @Override
-    protected void onFinishInflate() {
-        mDragView1 = findViewById(R.id.drag1);
-        mDragView2 = findViewById(R.id.drag2);
-    }
-
     private class DragHelperCallback extends ViewDragHelper.Callback {
 
         @Override
@@ -47,35 +38,11 @@ public class ViewDragHelperDemo extends LinearLayout{
             return true;
         }
 
-        @Override
-        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
-            invalidate();
-        }
-
-        @Override
-        public void onViewCaptured(View capturedChild, int activePointerId) {
-            super.onViewCaptured(capturedChild, activePointerId);
-        }
-
-        @Override
-        public void onViewReleased(View releasedChild, float xvel, float yvel) {
-            super.onViewReleased(releasedChild, xvel, yvel);
-        }
-
-        @Override
-        public void onEdgeTouched(int edgeFlags, int pointerId) {
-            super.onEdgeTouched(edgeFlags, pointerId);
-        }
-
-        @Override
-        public void onEdgeDragStarted(int edgeFlags, int pointerId) {
-            mDragHelper.captureChildView(mDragView1, pointerId);
-        }
 
         @Override
         public int clampViewPositionVertical(View child, int top, int dy) {
                 final int topBound = getPaddingTop();
-                final int bottomBound = getHeight() - mDragView1.getHeight();
+                final int bottomBound = getHeight() - child.getHeight();
 
                 final int newTop = Math.min(Math.max(top, topBound), bottomBound);
 
@@ -85,7 +52,7 @@ public class ViewDragHelperDemo extends LinearLayout{
         @Override
         public int clampViewPositionHorizontal(View child, int left, int dx) {
                 final int leftBound = getPaddingLeft();
-                final int rightBound = getWidth() - mDragView1.getWidth();
+                final int rightBound = getWidth() - child.getWidth();
 
                 final int newLeft = Math.min(Math.max(left, leftBound), rightBound);
 
