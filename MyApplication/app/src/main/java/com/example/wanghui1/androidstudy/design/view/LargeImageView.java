fb83ea199344a768a6dcfb2caecffe3948a5a802
wanghui1 <huia.wang@quantgroup.cn>
2017-01-19 18:14:45 +0800

    自定义View 局部图片

--- MyApplication/app/src/main/java/com/example/wanghui1/androidstudy/design/view/LargeImageView.java
+++ MyApplication/app/src/main/java/com/example/wanghui1/androidstudy/design/view/LargeImageView.java
@@ -1,8 +1,12 @@
 package com.example.wanghui1.androidstudy.design.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.example.wanghui1.androidstudy.R;
 
 /**
  * Created by wanghui on 2017/1/12.
@@ -10,6 +14,7 @@ import android.view.View;
  */
 
 public class LargeImageView extends View {
+
     public LargeImageView(Context context) {
         this(context, null);
     }
@@ -20,5 +25,8 @@ public class LargeImageView extends View {
 
     public LargeImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+//        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.LargeImageView);
+
     }
 }
